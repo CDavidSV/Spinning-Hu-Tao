8880cd5e0c24df4f00756769791f315b41b4e2b6
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2022-11-10 16:24:50 -0600

    Update 3
    - Added messages when reaching certain amount of spins.
    - Cookies save selected theme.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const txtCounter = document.getElementById('counter');
 const muteBtn = document.getElementById('mute-btn');
 const themeModeBtn = document.getElementById('theme-mode');
 const body = document.getElementById('body-theme');
+const messageContent = document.getElementById('content-msg');
 
 // Setup audio.
 var audio = new Audio('../assets/audio/Hu tao edit Im a Ghost - Audio Edit.mp3');
@@ -11,17 +12,36 @@ audio.loop = true;
 audio.volume = 0.6;
 
 //Variables.
-let rotations = 20320;
+let rotations = 0;
 let playClicked = false;
 let themeClicked = false;
+let counter = 0;
 const messages = [
-    { spins: "", msg: "" },
-    { spins: "", msg: "" },
-    { spins: "", msg: "" },
-    { spins: "", msg: "" },
-    { spins: "", msg: "" },
-    { spins: "", msg: "" },
+    { spins: 10, msg: "You like Hu Tao don't you." },
+    { spins: 25, msg: "Hu Tao Spiiiin!!!" },
+    { spins: 50, msg: "Half a hundred!" },
+    { spins: 100, msg: "100 Hu Tao spins!!" },
+    { spins: 200, msg: "You still here?" },
+    { spins: 500, msg: "Half a thousand spins... You must really like Hu Tao." },
+    { spins: 1000, msg: "Damn, I didn't even get to one thousand. There is no suprise you know." },
+    { spins: 10000, msg: "Holy, YOU REALLY LIKE HU TAO." },
+    { spins: 1000000, msg: "Man you should just work for miHoYo at this point." },
 ];
+
+// Load cookies.
+if (!document.cookie) {
+    setCookie();
+    document.cookie = `theme=${themeClicked}`;
+} else {
+    const cookiesObj = getCookie(document.cookie);
+
+    if (cookiesObj[0].value != 'false') {
+        body.style = "background-color: black;";
+        messageContent.style = "color: white;";
+        themeModeBtn.src = "../assets/images/light.svg";
+        themeClicked = true;
+    }
+}
 
 // Functions.
 function formatNumbers(number) {
@@ -44,10 +64,33 @@ function formatNumbers(number) {
     return numberStringRev;
 }
 
+function setCookie(name, value) {
+    document.cookie = `${name}=${value}`;
+}
+
+function getCookie(cookie) {
+    cookiesArr = cookie.split(' ');
+    cookliesObj = [];
+    for (cookie of cookiesArr) {
+        cookliesObj.push({ cookie: cookie.split("=")[0], value: cookie.split("=")[1] });
+    }
+    return cookliesObj;
+}
+
 // Listeners.
 spinningHuTao.addEventListener('animationiteration', () => {
     rotations++;
     txtCounter.textContent = formatNumbers(rotations);
+
+    let deleteMsg;
+    if (counter < messages.length && rotations >= messages[counter].spins) {
+        clearTimeout(deleteMsg);
+        messageContent.textContent = messages[counter].msg;
+        deleteMsg = setTimeout(() => {
+            messageContent.textContent = "";
+        }, 15000);
+        counter++;
+    }
 });
 
 muteBtn.addEventListener('click', () => {
@@ -67,10 +110,14 @@ themeModeBtn.addEventListener('click', () => {
     if (themeClicked) {
         body.style = "background-color: white;";
         themeModeBtn.src = "../assets/images/dark.svg";
+        messageContent.style = "color: black;";
         themeClicked = false;
+        setCookie("theme", false);
         return;
     }
     body.style = "background-color: black;";
     themeModeBtn.src = "../assets/images/light.svg";
+    messageContent.style = "color: white;";
     themeClicked = true;
+    setCookie("theme", true);
 });
