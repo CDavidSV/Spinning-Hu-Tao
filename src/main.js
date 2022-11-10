6c0fd0d0dbef583d372c44685909a86db132e678
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2022-11-10 11:15:57 -0600

    Update
    - Improved counter.
    - Image resizes to fit the screen resolution.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 const spinningHuTao = document.getElementById('Hu-Tao');
 const txtCounter = document.getElementById('counter');
-
 let rotations = 0;
+
+function formatNumbers(number) {
+    let numberString = "";
+    let numberStringRev = "";
+    const numbersArr = String(number).split("");
+
+    let counter = 1;
+    for (let i = numbersArr.length - 1; i > -1; i--) {
+        if (counter % 4 === 0) {
+            numberString += ','
+        }
+        numberString += numbersArr[i];
+        counter++;
+    }
+    for (let i = numberString.length - 1; i > -1; i--) {
+        numberStringRev += numberString[i];
+    }
+
+    return numberStringRev;
+}
+
 spinningHuTao.addEventListener('animationiteration', () => {
-    rotations++;    
-    txtCounter.textContent = rotations;
+    rotations++;
+    txtCounter.textContent = formatNumbers(rotations);
 });
